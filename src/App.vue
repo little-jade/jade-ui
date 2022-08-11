c17147d83a84f573357f5a969ae3f73ea156d233
little-jade <xiaoyu.peng@outlook.com>
2022-08-12 07:03:01 +0800

    delete some files

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <header></header>
+  <main></main>
 </template>
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 <style>
 @import "./assets/base.css";
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Jaui from "@/index";
-// import "@theme/bootstrap.scss"
 import "@theme/../src/index.scss";
 
 const app = createApp(App);
